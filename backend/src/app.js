477b40e0d28f98804afdfd2fd5c6d8a491d1d25a
RRodes3 <raulrodas939@gmail.com>
2026-06-05 20:41:50 -0600

    Peparacion del proyecto QrPlanta para produccion

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,23 @@ const importRoutes = require ('./routes/import.routes');
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173',
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error('Origen no permitido por CORS'));
+    },
+  })
+);
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
